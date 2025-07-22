47901e2a30380ae44a5cb730850f7cf22e15d981
Sneha Sakpal <snehasakpal35@gmail.com>
2025-07-23 00:40:56 +0530

    Add files via upload

---
+++ Array_reverse.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.util.Arrays;
+public class Array_reverse {
+    public static void main(String[] args) {
+        int[] arr = {10,20,30,40,50};
+        int start =0;
+        int end = arr.length-1;
+
+        while(start<end){
+            int temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
+
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
